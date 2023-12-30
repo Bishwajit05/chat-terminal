002238dceb94e2931fc5ebb4e9da9769ec2f942a
aditya <adithyachalla8@gmail.com>
2023-12-30 23:45:44 +0530

    wip of chat interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [userInput, setUserInput] = useState("");
+  const [responses, setResponses] = useState([]);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const res = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const { answer } = await res.json();
+    setResponses([...responses, { query: userInput, answer }] as any);
+    setUserInput("");
+  };
+
   return (
-    <main className="flex items-center justify-center">
-      <div>Hello</div>
-    </main>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <div>
+        {responses.map((res: any, index) => (
+          <div key={index}>
+            <p>
+              <strong>You:</strong> {res?.query}
+            </p>
+            <p>
+              <strong>AI:</strong> {res?.answer}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
